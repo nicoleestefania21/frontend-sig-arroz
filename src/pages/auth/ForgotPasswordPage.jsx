78fe9936030e02eb55f186a97f9b180ea8c6f1ea
andresnico44 <ramiroandresnc@ufps.edu.co>
2026-05-01 12:28:33 -0500

    Corrección de ruta

--- src/pages/auth/ForgotPasswordPage.jsx
+++ src/pages/auth/ForgotPasswordPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { API } from "../../api";
+import { API } from "../../config/api.js";
 import "../../styles/forgot-password.css";
 
 function ForgotPasswordPage() {
